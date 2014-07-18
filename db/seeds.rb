859a8a3bdb7edb270393134387ee5aed2ff5f05c
Marc Burt <marc.burt@gmail.com>
2014-07-18 18:51:29 +0100

    fixed db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Post.Create(title: First Post, content: "First Content")
+Post.Create(title: "First Post", content: "First Content")
