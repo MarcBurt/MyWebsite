e56ff1e5fb166d530138590d2318204007f7e2de
Marc Burt <marc.burt@gmail.com>
2014-08-26 13:19:22 +0100

    finally completed front page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,6 @@ class PostsController < ApplicationController
 
   def new
   	@post = Post.new
-
   end
 
   def create
@@ -27,6 +26,7 @@ class PostsController < ApplicationController
   	else
   		render "new"
   	end
+    
   end
 
   def edit
